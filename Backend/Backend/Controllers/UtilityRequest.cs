cba7c05e14c8964620260aac4f1475cf77eacba1
mymailvrn98 <mymailvrn98@gmail.com>
2020-04-11 20:06:30 +0300

    класс SearchRequest переделал в UtilityRequest, сделав его вспомогательным для любым запросов;
    создал контроллер для получения конкретного рецепта по его ID;
    создал два вспомогательных класса OutPutRecipe и OutPutStep, которые предназначены для отдачи во frontend;

---
+++ Backend/Backend/Controllers/UtilityRequest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    public class SearchRequest
+    {
+        public string name { get; set; }
+        public string mainIngredient { get; set; }
+        public string nationality { get; set; }
+        public string author { get; set; }
+    }
+
+    public class GetRequest
+    {
+        public int id { get; set; }
+    }
+
+    public class OutPutRecipe
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string mainIngredient { get; set; }
+        public string nationality { get; set; }
+        public string author { get; set; }
+        public List<OutPutStep> steps { get; set; }
+
+        public OutPutRecipe(int id, string name, string mainIngredient, string nationality, string author, List<OutPutStep> steps)
+        {
+            this.id = id;
+            this.name = name;
+            this.mainIngredient = mainIngredient;
+            this.nationality = nationality;
+            this.author = author;
+            this.steps = steps;
+        }
+    }
+
+    public class OutPutStep
+    {
+        public string ingredient { get; set; }
+        public string action { get; set; }
+        public string time { get; set; }
+
+        public OutPutStep(string ingredient, string action, string time)
+        {
+            this.ingredient = ingredient;
+            this.action = action;
+            this.time = time;
+        }
+    }
+}
