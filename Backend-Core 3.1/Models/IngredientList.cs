87a63c3c9a952dafb83c0716519f6ff835064ae4
neckbor <neckbor@mail.ru>
2020-04-17 15:04:02 +0300

    - добавил модель;

---
+++ Backend-Core 3.1/Models/IngredientList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend_Core_3._1.Models
+{
+    public partial class IngredientList
+    {
+        public int IdingredientList { get; set; }
+        public int Idrecipe { get; set; }
+        public int Idingredient { get; set; }
+        public string Amount { get; set; }
+
+        public virtual Ingredient IdingredientNavigation { get; set; }
+        public virtual Recipe IdrecipeNavigation { get; set; }
+    }
+}
---
+++ Backend-Core 3.1/Models/Nationality.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend_Core_3._1.Models
+{
+    public partial class Nationality
+    {
+        public Nationality()
+        {
+            Recipe = new HashSet<Recipe>();
+        }
+
+        public int Idnationality { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Recipe> Recipe { get; set; }
+    }
+}
