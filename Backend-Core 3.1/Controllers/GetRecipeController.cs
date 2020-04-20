61e407604280be825b217c77b404a5a0bcbcf8e4
neckbor <neckbor@mail.ru>
2020-04-20 19:50:52 +0300

    - добавил контроллер GetRecipe, заглушки всех методов;

---
+++ Backend-Core 3.1/Controllers/GetRecipeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Core_3._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetRecipeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult NextStep()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult PreviousStep()
+        {
+            return Ok();
+        }
+    }
+}
