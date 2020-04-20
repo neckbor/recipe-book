a6459240144bff9bf4b0c41bbdb956556a0c5efb
neckbor <neckbor@mail.ru>
2020-04-20 20:32:05 +0300

    - добавил класс для ответа сервера;

---
+++ Backend-Core 3.1/Models/BindingModels/RecipeBindingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Core_3._1.Models.BindingModels
+{
+    public class RecipeBindingModel
+    {
+        public int IDRecipe { get; set; }
+        public string Name { get; set; }
+        public string Nationality { get; set; }
+        public string FirstStep { get; set; }
+    }
+}
