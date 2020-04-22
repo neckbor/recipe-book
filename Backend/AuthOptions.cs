1cd50b08837c6cc75e4c786706ced07b30f49dca
neckbor <neckbor@mail.ru>
2020-04-22 23:21:10 +0300

    - добавл классы модели: пользователи, роли; - добавил класс конфигураци токенов; - удалил автоматически созданные студией классы;

---
+++ Backend/AuthOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer"; // издатель токена
+        public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+        const string KEY = "no1body_can_hack_YummYummY.Backend-matherf88ckers";   // ключ для шифрации
+        public const int LIFETIME = 30; // время жизни токена - 1 минута
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
