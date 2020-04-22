cb180c910bec03378eb2b653b07ff86bd3d8ae26
mymailvrn98 <mymailvrn98@gmail.com>
2020-04-22 21:43:59 +0300

    создал контроллер RecipeManager для добавления/изменения/удаления выбранного рецепта;
    создал вспомогательный класс FullInfoRecipeBindingModel для добавления/изменения рецепта;

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend
 {
