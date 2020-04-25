26ae6871a97f3deb859a1d9c60fcf10fe2a13391
alex2704 <34662549+alex2704@users.noreply.github.com>
2020-04-25 21:31:31 +0300

    Убрал ненужные комментарии;

--- frontend/src/app/recipe/recipe.component.ts
+++ frontend/src/app/recipe/recipe.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {Recipe} from '../models/Recipe';
 import {RecipeService} from '../services/recipeService';
 import {ActivatedRoute} from '@angular/router';
-import {Ingredient} from '../models/Ingredient';
-import {Step} from '../models/Step';
 
 @Component({
   selector: 'app-recipe',
@@ -16,8 +14,6 @@ export class RecipeComponent implements OnInit {
 
   recipe: Recipe;
   id: number;
-  completed = false;
-  state: string;
   ngOnInit() {
     this.id = this.route.snapshot.params.id;
     this.http.getRecipeById(this.id).subscribe(
@@ -27,11 +23,6 @@ export class RecipeComponent implements OnInit {
       },
       err => console.log('Произошла ошибка при получении рецепта', err)
     );
-    // this.recipe = new Recipe(1, 'яйцо', 'масло', 'Россия', 'alex', 30,
-    //   [new Ingredient(1, 'масло', '2'), new Ingredient(1, 'Яйцо', '3')],
-    //   [new Step(1, 1, 'поджарить'), new Step(2, 3, 'убрать'),
-    //     new Step(3, 2, 'полить маслом')]);
-    // console.log(this.recipe.ingredients[0].amount);
   }
   done() {
     console.log('рецепт завершен');
