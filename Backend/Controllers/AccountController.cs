67496058d1fd37a5e5311ac2100727439e13c54d
neckbor <neckbor@mail.ru>
2020-04-26 00:24:22 +0300

    регистрация: - добавил модель данных с формы регистрации; - реализовал метод регистрации пользователя;

--- Backend/Controllers/AccountController.cs
+++ Backend/Controllers/AccountController.cs
@@ -8,11 +8,11 @@ using Backend.Models;
 using Backend.Models.BindingModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Backend.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
     {
@@ -57,8 +57,7 @@ namespace Backend.Controllers
             {
                 var claims = new List<Claim>();
                 Claim c1 =  new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login);
-                string role = model.Role.Find(model.UserRole.Where(ur => ur.Iduser == 2).FirstOrDefault().Idrole).Name;
-                Claim c2 = new Claim(ClaimsIdentity.DefaultRoleClaimType, role);
+                Claim c2 = new Claim(ClaimsIdentity.DefaultRoleClaimType, model.Role.Find(user.Idrole).Name);
 
                 claims.Add(c1);
                 claims.Add(c2);
@@ -71,5 +70,50 @@ namespace Backend.Controllers
             // если пользователя не найдено
             return null;
         }
+
+        [HttpPost("api/[controller]/register")]
+        public IActionResult Register(RegisterBindingModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest();
+
+                LoginBindingModel user = AddUser(model);
+
+                return Token(user);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        private LoginBindingModel AddUser(RegisterBindingModel user)
+        {
+            using ModelDbContext model = new ModelDbContext();
+            if (model.User.Any(u => EF.Functions.Like(u.Login.ToLower(), user.login.ToLower())))
+                throw new Exception("Пользователь с таким логином уже существует");
+            if (model.User.Any(u => EF.Functions.Like(u.Email.ToLower(), user.email.ToLower())))
+                throw new Exception("Пользователь с такой почтой уже существует");
+
+            model.Add(new User()
+            {
+                Login = user.login,
+                Email = user.email,
+                PassworgHash = user.password,
+                Idrole = 1,
+                EmailConfirmed = false,
+                IdroleNavigation = model.Role.Find(1)
+            });
+            model.SaveChanges();
+
+            return new LoginBindingModel()
+            {
+                login = user.login,
+                password = user.password
+            };
+        }
+
     }
 }
