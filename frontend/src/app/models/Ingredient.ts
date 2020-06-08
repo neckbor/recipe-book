50c2609ea392aafa2e5e825adfcc367a900caf38
alex2704 <34662549+alex2704@users.noreply.github.com>
2020-06-08 14:20:39 +0300

    Исправил отображение ингридиентов в рецепте;

--- frontend/src/app/models/Ingredient.ts
+++ frontend/src/app/models/Ingredient.ts
@@ -1,10 +1,10 @@
 export class Ingredient {
   idIngredientList: number;
-  ingredient: string;
+  name: string;
   amount: string;
   constructor(idIngredientList: number, ingredient: string, amount: string) {
     this.idIngredientList = idIngredientList;
-    this.ingredient = ingredient;
+    this.name = ingredient;
     this.amount = amount;
   }
 }
